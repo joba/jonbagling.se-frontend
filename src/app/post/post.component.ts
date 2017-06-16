fea3942fc974d7047c63cd54bdfa5038c3f88845
Jon Bågling <bagling@gmail.com>
2017-06-16 15:58:41 +0200

    adds loader, more css

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -12,13 +12,22 @@ export class PostComponent implements OnInit {
   constructor(private postService: PostService) { }
   private loading: boolean = true;
   private allPosts: Array<Post>;
+  private activePosts: Array<Post>;
 
   ngOnInit() {
   	this.postService.getPosts().subscribe(posts => {
   		console.log(posts);
   		this.allPosts = posts;
   		this.loading = false;
+      this.filterPosts(this.allPosts);
   	});
+  }
+
+  private filterPosts(posts) {
+    posts.forEach((post) => {
+      // console.log(this.activePosts);
+      // if(post.live) this.activePosts.push(post);
+    });
   }
 
 }
